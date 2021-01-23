b604997cc43c137b45b9b38b8f43ada1c0dc5e50
Nirmal K <cyberwizard1001@gmail.com>
2021-01-23 11:25:45 +0530

    DS linked lists

--- DS/linked_list.py
+++ DS/linked_list.py
@@ -1,93 +1,204 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 22 13:58:00 2021
+class SLList:
 
-@author: nirmalk
-"""
-
-
-class singly_linked_lists:
     class node:
-        def __init__(self):
-            self.element = None
+        def __init__(self,data):
+            self.element = data
             self.next = None
-
+        
     def __init__(self):
-        self.head = self.node()
+        self.head = self.node(None)
         self.sz = 0
+          
+          
+    def insertLast(self,data):
+        #@start-editable@
 
-    def printList(self):
-        tnode = self.head
-
-        while tnode.next != None:
-            print(tnode.element, end="-->")
-            tnode = tnode.next
+        curnode = self.node()
+        curnode = self.head
+        while curnode.next!=None:
+            curnode = curnode.next
+        
+        curnode.next = data
+        self.sz+=1
+        
+        #@end-editable@
         return
 
-    def size_of_list(self):
-        return self.sz
+    def insertFirst(self,data):
+        #@start-editable@
 
-    def isempty(self):
-        return self.sz == 0
-
-    def insertFirst(self, u):
-        u.next = self.head
-        self.head = u
-        self.sz += 1
+        data.next = self.head
+        self.head = data
+        self.s+=1
+        
+        #@end-editable@
         return
+     
+    def deleteFirst(self):
 
-    def delete_first(self):
-        if self.size_of_list() == 0:
-            return "List Empty"
-        elif self.size_of_list() == 1:
+        #@start-editable@
+
+        if self.size()==0:
+            return "List empty"
+            
+        elif self.size() == 1:
             self.head.element = None
-            self.sz -= 1
+            self.sz-=1
+            
         else:
             temp = self.node()
             temp = self.head
             head = self.head.next
             temp.next = None
-            self.sz -= 1
+            self.sz-=1
+        
+        #@end-editable@
+        return
 
-    def delete_last(self):
-        if self.size_of_list() == 0:
-            return "List Empty"
-        elif self.size_of_list() == 1:
+    def deleteLast(self):
+        #@start-editable@
+
+        if self.size() == 0:
+            return "List empty"
+            
+        elif self.size()==1:
             self.head.element = None
-            self.sz = -1
+            self.sz=-1
+            
         else:
             curnode = self.node()
-            while curnode.next.next != None:
+            curnode = self.head
+            while curnode.next.next!=None:
                 curnode = curnode.next
-            curnode.next = None
+            curnode.next = None;
+        
+        #@end-editable@
+        return
 
-    def delete_after(self,v):
-        if self.size_of_list() == 0:
-            return "List Empty"
+          
+    def printList(self):
+        if (self.isEmpty()):
+            print ("List Empty")
         else:
-            curnode = self.node()
-            while curnode.element!=v.element:
-                curnode = curnode.next
-            curnode.next = None
+            tnode = self.head
+            while tnode!= None:
+                print(tnode.element,end="->")
+                tnode = tnode.next
+            print("null")
         return
+     
+    def findNode(self, val):
+        #@start-editable@
 
-    def insert_after(self,v):
+        curnode = self.node()
+		
+        while(curnode.element!=val):
+            curnode = curnode.next
 
+        return curnode.element
+        
+        #@end-editable@
+        return None
+        
+    def getHead(self):
+        #@start-editable@
+
+        return self.head.element
+        
+        #@end-editable@
+     
+    def isEmpty(self):
+        #@start-editable@
+
+        return(self.sz==0)
+        
+        
+        #@end-editable@
+
+    def size(self):
+        #@start-editable@
+
+        return self.sz
+        
+        #@end-editable@
+		  
+    def getLastNode(self):
+        #@start-editable@
+
+        curnode = self.node()
+        curnode = self.head
+        while curnode.next!=None:
+            curnode = curnode.next
+            
+        return curnode.element
+        
+        #@end-editable@
+		  
+    def delKth(self, k):
+          
+        #@start-editable@
+
+        curnode = self.node()
+        curnode = self.head
+        count = 0
+
+        while count<k-1:
+            curnode = curnode.next
+            count+=1
+            
+        curnode.next = curnode.next.next
+        self.sz-=1
+        
+        #@end-editable@
+     
+    def swapAdj(self):
+          
+        #@start-editable@
+
+        
         return
+        
+        #@end-editable@
+     
+def testSLL():
+    sll = SLList()
+    inputs=int(input())
+    while inputs>0:
+        command=input()
+        operation=command.split()
+        if(operation[0]=="S"):
+            print(sll.size())
+        elif(operation[0]=="I"):
+            print(sll.isEmpty())
+        elif(operation[0]=="AF"):
+            sll.insertFirst(int(operation[1]))
+            sll.printList()
+        elif(operation[0]=="AL"):
+            sll.insertLast(int(operation[1]))
+            sll.printList()
+        elif(operation[0]=="RF"):
+            sll.deleteFirst()
+            sll.printList()
+        elif(operation[0]=="RL"):
+            sll.deleteLast()
+            sll.printList()
+        elif(operation[0]=="F"):
+            print(sll.getHead())
+        elif(operation[0]=='L'):
+            print(sll.getLastNode())
+        elif(operation[0]=='FIND'):
+            print(sll.findNode(int(operation[1])))
+        elif(operation[0]=='DK'):
+            sll.delKth(int(operation[1]))
+            sll.printList()
+        elif(operation[0]=='SA'):
+            sll.swapAdj()
+            sll.printList()
+        inputs-=1
 
-    def find_node(self,n):
 
-        return
+def main():
+    testSLL()
 
-
-list1 = singly_linked_lists()
-print("size of the list : " + str(list1.size_of_list()))
-print("is empty : " + str(list1.isempty()))
-
-for i in range(5):
-    curnode = list1.node()
-    curnode.element = i
-    list1.insertFirst(curnode)
-print("*****")
-list1.printList()
+if __name__ == '__main__':
+    main()

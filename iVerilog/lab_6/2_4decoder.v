1503f78678a3ea4bfb01b739bcd0af189c28c8dd
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-03-18 15:24:21 +0530

    Changes to 24decoder

--- iVerilog/lab_6/2_4decoder.v
+++ iVerilog/lab_6/2_4decoder.v
@@ -9,28 +9,28 @@ not Gate1 (NA, A);
 not Gate2 (NB, B);
 not Gate3 (NE, en);
 
-nand G4 (D[0], NA, NB, NE );
-nand G5 (D[1], NA, B, NE );
-nand G6 (D[2], A, NB, NE );
-nand G7 (D[3], A, B, NE );
+nand Gate4 (D[0], NA, NB, NE );
+nand Gate5 (D[1], NA, B, NE );
+nand Gate6 (D[2], A, NB, NE );
+nand Gate7 (D[3], A, B, NE );
 
 initial
 begin
      $monitor("Enable=%b, A=%b, B=%b, Y0=%b, Y1=%b, Y2=%b, Y3=%b", en, A, B, D[0], D[1], D[2], D[3]);
 
-     $display("This is when the enable input is 1");
+     $display("EN=1");
 
-     e = 1;w1 = 0;w0 = 0; #10;
-     e = 1;w1 = 0;w0 = 1; #10;
-     e = 1;w1 = 1;w0 = 0; #10;
-     e = 1;w1 = 1;w0 = 1; #10;
+     en = 1;B = 0;A = 0; #10;
+     en = 1;B = 0;A = 1; #10;
+     en = 1;B = 1;A = 0; #10;
+     en = 1;B = 1;A = 1; #10;
 
-     $display("This is when the enable input is 0 ");
+     $display("EN=0");
 
-     e = 0;w1 = 0;w0 = 0; #10;
-     e = 0;w1 = 0;w0 = 1; #10;
-     e = 0;w1 = 1;w0 = 0; #10;
-     e = 0;w1 = 1;w0 = 1; #10;
+     en = 0;B = 0;A = 0; #10;
+     en = 0;B = 0;A = 1; #10;
+     en = 0;B = 1;A = 0; #10;
+     en = 0;B = 1;A = 1; #10;
      $finish;
 end
 

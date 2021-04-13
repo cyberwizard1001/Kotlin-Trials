f4acb778d21194a2753509d3f67edf80e2b64720
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-04-13 09:46:42 +0530

    completed all traversals

--- DS/self-practise/binary_tree.py
+++ DS/self-practise/binary_tree.py
@@ -14,6 +14,31 @@ class BinaryTree(object):
             self.PreOrderTraversal(self,v.leftchild)
             self.PreOrderTraversal(self,v.rightchild)
 
+        return
+
+    def InOrderTraversal(self,v):
+        if v!=None:
+            self.InOrderTraversal(self,v.leftchild)
+            print(v.value,end=" ")
+            self.InOrderTraversal(self,v.rightchild)
+
+        return 
+
+    def PostOrderTraversal(self,v):
+        if v!=None:
+            self.PostOrderTraversal(self,v.leftchild)
+            self.PostOrderTraversal(self,v.rightchild)
+            print(v.value,end=" ")
+
+        return
+
+    def PrintTree(self,nlist):
+        for i in range(len(nlist)):
+            if (nlist[i] != None):
+                print(nlist[i].element,end=" ")
+            else:
+                print(-1)
+
 tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
